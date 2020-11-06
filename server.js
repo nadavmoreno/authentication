3b7b619763daf7a3e4ea729eee786fb07c4a6c7e
nadavmoreno <>
2020-11-06 14:22:28 +0200

    cookie

--- server.js
+++ server.js
@@ -1,53 +1,34 @@
 const express = require('express');
-const chalk = require('chalk');
 const debug = require('debug')('app:server');
 const morgan = require('morgan');
 const path = require('path');
-const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
 
 const app = express();
 const port = 4003;
 
 app.use(morgan('tiny'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(session({secret: 'nm'}));
-require('./src/config/passport.js')(app);
 
-app.use(express.static(path.join(__dirname, '/public/')));
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
 
-const authRouter = require('./src/routes/authRoutes')();
-const memberRouter = require('./src/routes/memberRoutes')();
-
-app.use('/auth', authRouter);
-app.use('/member', memberRouter);
-
-app.get('/', (req, res) => {
-    res.render('pages/login', {
-        pageTitle: 'Login',
-        userName: req.user ? req.user.username : null,
-    });
+app.get("/", (req, res) => {
+    console.log(req.cookies)
+    let text = 'Hi there, unknown user, to set the cookie please go to http://127.0.0.1:4003/cookie'
+    if(req.cookies.name) {
+        text = `Hi there, ${req.cookies.name}, to delete the cookie go to http://127.0.0.1:4003/delete`;
+    }
+    res.send(text);
 });
 
-app.get('/registration', (req, res) => {
-    res.render('pages/registration', {
-        pageTitle: 'Registration',
-        userName: req.user ? req.user.username : null,
-    });
+app.get("/cookie", (req, res) => {
+    res.cookie('name' , 'nadav',{ expires: new Date(Date.now() + 900000), httpOnly: true }).send('Cookie is set, name="nadav", go to http://127.0.0.1:4003 to see the change');
+});
+
+app.get("/delete", (req, res) => {
+    res.clearCookie("name").send('Cookie is deleted, go to http://127.0.0.1:4003 to see the change');
 });
 
 const start = async () => {
-    await mongoose.connect(
-        'mongodb://127.0.0.1/freelanceApp',
-        {useNewUrlParser: true, useFindAndModify: false},
-    );
-    debug('Connected to db server');
-
     app.listen(port, () => {
         debug(`listening on port ${port}`);
     });
